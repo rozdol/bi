91999a2dbb393ebc89eff55755a7d85b07a60870
Alex Titoff <rozdol@gmail.com>
2018-09-11 17:53:35 +0300

    docs:DOWNLOADX

--- src/Utils/Html.php
+++ src/Utils/Html.php
@@ -609,6 +609,9 @@ class Html
         if (($GLOBALS[act]=='save')&&($GLOBALS[what]=='processdata')&&($this->readRQ('formaction')=='invoices:DOWNLOADX')) {
             $GLOBALS[plain]=1;
         }
+        if (($GLOBALS[act]=='save')&&($GLOBALS[what]=='processdata')&&($this->readRQ('formaction')=='docs:DOWNLOADX')) {
+            $GLOBALS[plain]=1;
+        }
         if (($GLOBALS[act]=='save')&&($GLOBALS[what]=='shoppingcart')) {
             $GLOBALS[plain]=1;
         }
