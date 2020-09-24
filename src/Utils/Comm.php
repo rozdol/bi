43a19094f91426b04bbb3e6a475c8ff3a194fcce
Alexander Titov <rozdol@gmail.com>
2020-09-24 17:06:54 +0300

    Add sendIcalEvent

--- src/Utils/Comm.php
+++ src/Utils/Comm.php
@@ -28,6 +28,149 @@ class Comm
         $this->utils = Utils::getInstance();
     }
 
+    public function sendIcalEvent($from_name, $from_address, $to_name, $to_address, $startTime, $endTime, $subject, $description, $location)
+    {
+        $domain = 'google.com';
+
+        //Create Email Headers
+        $mime_boundary = "----Meeting Booking----".MD5(TIME());
+
+        $headers = "From: ".$from_name." <".$from_address.">\n";
+        $headers .= "Reply-To: ".$from_name." <".$from_address.">\n";
+        $headers .= "MIME-Version: 1.0\n";
+        $headers .= "Content-Type: multipart/alternative; boundary=\"$mime_boundary\"\n";
+        $headers .= "Content-class: urn:content-classes:calendarmessage\n";
+
+        //Create Email Body (HTML)
+        $message = "--$mime_boundary\r\n";
+        $message .= "Content-Type: text/html; charset=UTF-8\n";
+        $message .= "Content-Transfer-Encoding: 8bit\n\n";
+        $message .= "<html>\n";
+        $message .= "<body>\n";
+        $message .= '<p>Dear '.$to_name.',</p>';
+        $message .= '<p>'.$description.'</p>';
+        $message .= "</body>\n";
+        $message .= "</html>\n";
+        $message .= "--$mime_boundary\r\n";
+
+
+        $ical = 'BEGIN:VCALENDAR' . "\r\n" .
+        #'PRODID:-//Microsoft Corporation//Outlook 10.0 MIMEDIR//EN' . "\r\n" .
+        'VERSION:2.0' . "\r\n" .
+        'METHOD:REQUEST' . "\r\n" .
+        'PRODID:-//Apple Inc.//Mac OS X 10.15.6//EN' . "\r\n" .
+        'CALSCALE:GREGORIAN' . "\r\n" .
+        'BEGIN:VTIMEZONE' . "\r\n" .
+        'TZID:Asia/Nicosia' . "\r\n" .
+        'BEGIN:STANDARD' . "\r\n" .
+        'TZOFFSETFROM:+0300' . "\r\n" .
+        'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU' . "\r\n" .
+        'DTSTART:19981025T040000' . "\r\n" .
+        'TZNAME:GMT+2' . "\r\n" .
+        'TZOFFSETTO:+0200' . "\r\n" .
+        'END:STANDARD' . "\r\n" .
+        'BEGIN:DAYLIGHT' . "\r\n" .
+        'TZOFFSETFROM:+0200' . "\r\n" .
+        'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU' . "\r\n" .
+        'DTSTART:19990328T030000' . "\r\n" .
+        'TZNAME:GMT+3' . "\r\n" .
+        'TZOFFSETTO:+0300' . "\r\n" .
+        'END:DAYLIGHT' . "\r\n" .
+        'END:VTIMEZONE' . "\r\n" .
+        'BEGIN:VEVENT' . "\r\n" .
+        'ORGANIZER;CN="'.$from_name.'":MAILTO:'.$from_address. "\r\n" .
+        'ATTENDEE;CN="'.$to_name.'";ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:'.$to_address. "\r\n" .
+        'TRANSP:OPAQUE' . "\r\n" .
+        'DTEND;TZID=Asia/Nicosia:'.date("Ymd\THis", strtotime($endTime)). "\r\n" .
+        'UID:'.date("Ymd\TGis", strtotime($startTime)).rand()."@".$domain."\r\n" .
+        'DTSTAMP:' . date("Ymd\TGis") . "\r\n" .
+        'DESCRIPTION:' . "\r\n" .
+        'STATUS:TENTATIVE' . "\r\n" .
+        'SEQUENCE:1' . "\r\n" .
+        'X-APPLE-TRAVEL-ADVISORY-BEHAVIOR:AUTOMATIC' . "\r\n" .
+        'SUMMARY:' . $subject . "\r\n" .
+        'LOCATION:' . $location . "\r\n" .
+        'LAST-MODIFIED:' . date("Ymd\TGis") . "\r\n" .
+        'CREATED:20200918T100012Z' . "\r\n" .
+        'DTSTART;TZID=Asia/Nicosia:'.date("Ymd\THis", strtotime($startTime)). "\r\n" .
+        // 'BEGIN:VALARM' . "\r\n" .
+        // 'X-WR-ALARMUID:A5051B64-8394-4DDA-A31E-3A57FDA5AE4F' . "\r\n" .
+        // 'UID:A5051B64-8394-4DDA-A31E-3A57FDA5AE4F' . "\r\n" .
+        // 'TRIGGER;VALUE=DATE-TIME:19760401T005545Z' . "\r\n" .
+        // 'ACKNOWLEDGED:20200921T084717Z' . "\r\n" .
+        // 'X-APPLE-DEFAULT-ALARM:TRUE' . "\r\n" .
+        // 'ACTION:NONE' . "\r\n" .
+        // 'END:VALARM' . "\r\n" .
+        // 'BEGIN:VALARM' . "\r\n" .
+        // 'X-WR-ALARMUID:DC23BB85-6474-450A-986B-4EFCFC3D2900' . "\r\n" .
+        // 'UID:DC23BB85-6474-450A-986B-4EFCFC3D2900' . "\r\n" .
+        // 'DESCRIPTION:This is an event reminder' . "\r\n" .
+        // 'ACKNOWLEDGED:20200921T084717Z' . "\r\n" .
+        // 'TRIGGER:-PT1H' . "\r\n" .
+        // 'ACTION:DISPLAY' . "\r\n" .
+        // 'END:VALARM' . "\r\n" .
+        'CLASS:PUBLIC'. "\r\n" .
+        'PRIORITY:5'. "\r\n" .
+        'BEGIN:VALARM' . "\r\n" .
+        'TRIGGER:-PT15M' . "\r\n" .
+        'ACTION:DISPLAY' . "\r\n" .
+        'DESCRIPTION:Reminder' . "\r\n" .
+        'END:VALARM' . "\r\n" .
+
+        'END:VEVENT' . "\r\n" .
+        'END:VCALENDAR' . "\r\n";
+
+        $ical_old = 'BEGIN:VCALENDAR' . "\r\n" .
+        'PRODID:-//Microsoft Corporation//Outlook 10.0 MIMEDIR//EN' . "\r\n" .
+        'VERSION:2.0' . "\r\n" .
+        'METHOD:REQUEST' . "\r\n" .
+        'BEGIN:VTIMEZONE' . "\r\n" .
+        'TZID:Eastern Time' . "\r\n" .
+        'BEGIN:STANDARD' . "\r\n" .
+        'DTSTART:20091101T020000' . "\r\n" .
+        'RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=1SU;BYMONTH=11' . "\r\n" .
+        'TZOFFSETFROM:-0400' . "\r\n" .
+        'TZOFFSETTO:-0500' . "\r\n" .
+        'TZNAME:EST' . "\r\n" .
+        'END:STANDARD' . "\r\n" .
+        'BEGIN:DAYLIGHT' . "\r\n" .
+        'DTSTART:20090301T020000' . "\r\n" .
+        'RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=2SU;BYMONTH=3' . "\r\n" .
+        'TZOFFSETFROM:-0500' . "\r\n" .
+        'TZOFFSETTO:-0400' . "\r\n" .
+        'TZNAME:EDST' . "\r\n" .
+        'END:DAYLIGHT' . "\r\n" .
+        'END:VTIMEZONE' . "\r\n" .
+        'BEGIN:VEVENT' . "\r\n" .
+        'ORGANIZER;CN="'.$from_name.'":MAILTO:'.$from_address. "\r\n" .
+        'ATTENDEE;CN="'.$to_name.'";ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:'.$to_address. "\r\n" .
+        'LAST-MODIFIED:' . date("Ymd\TGis") . "\r\n" .
+        'UID:'.date("Ymd\TGis", strtotime($startTime)).rand()."@".$domain."\r\n" .
+        'DTSTAMP:'.date("Ymd\TGis"). "\r\n" .
+        'DTSTART;TZID="Eastern Time":'.date("Ymd\THis", strtotime($startTime)). "\r\n" .
+        'DTEND;TZID="Eastern Time":'.date("Ymd\THis", strtotime($endTime)). "\r\n" .
+        'TRANSP:OPAQUE'. "\r\n" .
+        'SEQUENCE:1'. "\r\n" .
+        'SUMMARY:' . $subject . "\r\n" .
+        'LOCATION:' . $location . "\r\n" .
+        'CLASS:PUBLIC'. "\r\n" .
+        'PRIORITY:5'. "\r\n" .
+        'BEGIN:VALARM' . "\r\n" .
+        'TRIGGER:-PT15M' . "\r\n" .
+        'ACTION:DISPLAY' . "\r\n" .
+        'DESCRIPTION:Reminder' . "\r\n" .
+        'END:VALARM' . "\r\n" .
+        'END:VEVENT'. "\r\n" .
+        'END:VCALENDAR'. "\r\n";
+        $message .= 'Content-Type: text/calendar;name="event.ics";method=REQUEST'."\n";
+        $message .= "Content-Transfer-Encoding: 8bit\n\n";
+        $message .= $ical;
+
+        $mailsent = mail($to_address, $subject, $message, $headers);
+
+        return ($mailsent)?(true):(false);
+    }
+
     public function send_mail_aws($sender_email = '', $recipient_emails, $subject = 'email', $html_body = '', $plaintext_body='',$attachments = []){
 
         $data_dir=getenv('DATA_DIR');
